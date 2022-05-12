60a6ffba09b3b0f6d9fce278dad6061859f1cd54
ko_matsumoto <43021504+komat346@users.noreply.github.com>
2022-05-12 17:04:27 +0900

    modify directory

--- LForM/LForM_ilm_policy.sh
+++ LForM/LForM_ilm_policy.sh
@@ -1,6 +1,6 @@
-PASS=`cat LForM/LForM_install.log | grep "The generated password" | awk '{print $11}'`
+PASS=`cat src/install.log | grep "The generated password" | awk '{print $11}'`
 
-curl --cacert /etc/kibana/certs/elasticsearch-ca.pem -u elastic:$PASS -H "kbn-xsrf: reporting" -H 'Content-Type: application/json' -XPUT "https://localhost:9200/_ilm/policy/lform_ilm_policy_001" -d '
+curl --cacert /etc/kibana/certs/elasticsearch-ca.pem -u elastic:$PASS -H "kbn-xsrf: reporting" -H 'Content-Type: application/json' -XPUT "https://localhost:9200/_ilm/policy/forti_ilm_policy_001" -d '
 {
   "policy": {
     "phases": {
