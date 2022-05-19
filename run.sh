942d8f47b4a0d7b1ed2393b9b124c654db2a7640
TomTakahashi <tom_takahashi@ap-com.co.jp>
2022-05-19 17:43:05 +0900

    Fix/installer (#8)
    * modify directory
    
    * modify directory
    
    * modify directory
    
    * Modify directory and install script
    
    * Modify scripts
    
    * Merge file for Palo into repository
    
    * Modify scripts
    
    * fix typo
    
    Co-authored-by: ko_matsumoto <43021504+komat346@users.noreply.github.com>

---
+++ run.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+read -rsp 'Password: ' ES_PASS
+
+systemctl start td-agent.service
+systemctl start elasticsearch.service
+sleep 120
+
+curl --cacert /etc/elasticsearch/certs/ca/ca.crt  -u elastic:$ES_PASS -H "Content-Type: application/json" -XPOST https://localhost:9200/_security/user/kibana_system/_password -d'
+{
+  "password" : "kibana_pw"
+}'
+
+systemctl start kibana.service
+sleep 120
+
+systemctl status td-agent.service
+systemctl status elasticsearch.service
+systemctl status kibana.service
+
+
+echo "**********************"
+echo "completed."
+echo "**********************"
